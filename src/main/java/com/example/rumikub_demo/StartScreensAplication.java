43a6f036ee84db2be11d6f33b08456e5e682695f
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-09-20 13:54:20 +0200

    Creating scenes and interactions between them

---
+++ src/main/java/com/example/rumikub_demo/StartScreensAplication.java
@@ -0,0 +1,68 @@
+package com.example.rumikub_demo;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartScreensAplication extends Application {
+    @FXML
+    private ComboBox<String> playerChoiceComboBox;
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void handlePlayVsAIAction(ActionEvent event) {
+    }
+
+    public void handleMultiplayerAction(ActionEvent event) {
+        switchScene("SelectPlayerScene.fxml", event);
+    }
+    public void handleBackToStart(ActionEvent event) {
+        switchScene("Start.fxml", event);
+    }
+
+    public void handlePlayerChoice(ActionEvent event){
+        switch (playerChoiceComboBox.getValue()) {
+            case "2 Players" -> {
+                System.out.println("Starting a game for 2 players");
+                switchScene("TwoPeopleNameInputScene.fxml", event);
+            }
+            case "3 Players" -> {
+                System.out.println("Starting a game for 3 players");
+                switchScene("ThreePeopleNameInputScene.fxml", event);
+            }
+            case "4 Players" -> {
+                System.out.println("Starting a game for 4 players");
+                switchScene("FourPlayerNameInput.fxml", event);
+            }
+        }
+    }
+
+    public void switchScene(String sceneName, ActionEvent event){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(sceneName));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
