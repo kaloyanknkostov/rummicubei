63e8d3099868526282c90bc27ce716f501247ef0
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-10-04 12:40:54 +0200

    starting GUI

--- src/main/java/com/gameEngine/GameRunner.java
+++ src/main/java/com/gameEngine/GameRunner.java
@@ -1,7 +1,11 @@
 package com.gameEngine;
 
+import com.example.GUI.StartScreensApplication;
+
 public class GameRunner {
+    static StartScreensApplication startScreensApplication = new StartScreensApplication();
     public static void main(String[] args) {
+        startScreensApplication.main(args);
         GameEngine game = new GameEngine(2,2);
         game.gameLoop();
     }
