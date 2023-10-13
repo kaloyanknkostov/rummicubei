10bee4e98fe09d9e32378b4edf6854df76ba8b42
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-10-13 14:39:26 +0200

    drag and drop

--- src/main/java/com/example/GUI/StartScreensApplication.java
+++ src/main/java/com/example/GUI/StartScreensApplication.java
@@ -1,6 +1,9 @@
 package com.example.GUI;
 
+import javafx.scene.input.DragEvent;
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +12,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
 
 import javafx.scene.image.Image;
@@ -29,20 +35,6 @@ public class StartScreensApplication extends Application {
     public TextField fourthPlayerName;
     @FXML
     private ComboBox<String> playerChoiceComboBox;
-    public static StartScreensApplication activeController;
-    private Parent root;
-    private final GameModel gameModel = GameModel.getInstance();
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        instance = this;
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Start.fxml")));
-        Scene scene = new Scene(root);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
     @FXML
     ImageView p00;
     @FXML
@@ -83,13 +75,26 @@ public class StartScreensApplication extends Application {
     ImageView p90;
     @FXML
     ImageView p91;
-    private static StartScreensApplication instance;
 
-    // Step 1: Private constructor
+    private static StartScreensApplication instance;
+    public static StartScreensApplication activeController;
+    private Parent root;
+    private final GameModel gameModel = GameModel.getInstance();
+    private final ObjectProperty<ImageView> dragSource = new SimpleObjectProperty<>();
+
     public StartScreensApplication() {
     }
 
-    // Step 3: Public static method to get the instance
+    @Override
+    public void start(Stage stage) throws IOException {
+        instance = this;
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Start.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static StartScreensApplication getInstance() {
         if (instance == null) {
             instance = new StartScreensApplication();
@@ -105,34 +110,6 @@ public class StartScreensApplication extends Application {
             switchScene("GamePane.fxml", event);
         }
     }
-
-    public void playerTurn() {
-        try {
-            int numOfTiles = gameModel.getCurrentPlayer().getDeckOfTiles().size();
-
-            ImageView[] playerBoard = {
-                    activeController.p00, activeController.p01,
-                    activeController.p10, activeController.p11,
-                    activeController.p20, activeController.p21,
-                    activeController.p30, activeController.p31,
-                    activeController.p40, activeController.p41,
-                    activeController.p50, activeController.p51,
-                    activeController.p60, activeController.p61,
-                    activeController.p70, activeController.p71,
-                    activeController.p80, activeController.p81,
-                    activeController.p90, activeController.p91
-            };
-
-            for (int i = 0; i < Math.min(numOfTiles, playerBoard.length); i++) {
-                String tileImage = gameModel.getCurrentPlayer().getDeckOfTiles().get(i).getPicture();
-                playerBoard[i].setImage(new Image(tileImage)); // I've also changed the image to be set from the tileImage variable.
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public void handleSingleAction(ActionEvent event) {
         switchScene("ErrorAI.fxml", event);
@@ -220,4 +197,85 @@ public class StartScreensApplication extends Application {
         }
     }
 
+    public void playerTurn() {
+        try {
+            int numOfTiles = gameModel.getCurrentPlayer().getDeckOfTiles().size();
+
+            ImageView[] playerBoard = {
+                    activeController.p00, activeController.p01,
+                    activeController.p10, activeController.p11,
+                    activeController.p20, activeController.p21,
+                    activeController.p30, activeController.p31,
+                    activeController.p40, activeController.p41,
+                    activeController.p50, activeController.p51,
+                    activeController.p60, activeController.p61,
+                    activeController.p70, activeController.p71,
+                    activeController.p80, activeController.p81,
+                    activeController.p90, activeController.p91
+            };
+
+            for (int i = 0; i < playerBoard.length; i++) {
+                if (i < numOfTiles) {
+                    String tileImage = gameModel.getCurrentPlayer().getDeckOfTiles().get(i).getPicture();
+                    playerBoard[i].setImage(new Image(tileImage));
+                    playerBoard[i].setVisible(true);
+                } else {
+                    playerBoard[i].setVisible(false);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        initializeDragAndDrop();
+    }
+
+    private void initializeDragAndDrop() {
+        ImageView[] playerBoard = {
+                activeController.p00, activeController.p01,
+                activeController.p10, activeController.p11,
+                activeController.p20, activeController.p21,
+                activeController.p30, activeController.p31,
+                activeController.p40, activeController.p41,
+                activeController.p50, activeController.p51,
+                activeController.p60, activeController.p61,
+                activeController.p70, activeController.p71,
+                activeController.p80, activeController.p81,
+                activeController.p90, activeController.p91
+        };
+
+        for (ImageView imageView: playerBoard) {
+            imageView.setOnDragDetected(event -> {
+                Dragboard db = imageView.startDragAndDrop(TransferMode.MOVE);
+                ClipboardContent content = new ClipboardContent();
+                content.putImage(imageView.getImage());
+                db.setContent(content);
+                dragSource.set(imageView);
+                event.consume();
+            });
+
+            imageView.setOnDragOver(event -> {
+                if (event.getGestureSource() != imageView && event.getDragboard().hasImage()) {
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }
+                event.consume();
+            });
+
+            imageView.setOnDragDropped(event -> {
+                ImageView source = dragSource.get();
+                if (source != null && source != imageView) {
+                    Image tempImage = imageView.getImage();
+                    imageView.setImage(source.getImage());
+                    source.setImage(tempImage);
+                    event.setDropCompleted(true);
+                    dragSource.set(null);
+                } else {
+                    event.setDropCompleted(false);
+                }
+                event.consume();
+            });
+
+            imageView.setOnDragDone(DragEvent::consume);
+        }
+    }
 }
