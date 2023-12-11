71861bd123f063dfac201f9007c6bba9c83ba81a
Frederik Grüneberg <82397734+fregru@users.noreply.github.com>
2023-12-11 23:33:36 +0100

    Add data transform tests

--- machine_learning/dataset.py
+++ machine_learning/dataset.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 from torch.utils.data import Dataset
-from PIL import Image
 
 
 # Create Dataset class which inherits from the PyTorch dataset class
@@ -26,3 +25,33 @@ class TileDataset(Dataset):
         # Get the label corresponding to the image
         label = self.img_labels.iloc[idx, 1]
         return image, label
+
+
+def tile_number2string(tile: int):
+    tile_index = np.floor_divide(tile, 13)
+    colors = {0: "blue", 1: "red", 2: "yellow", 3: "black"}
+    # if colors
+    pass
+
+
+def tile_string2number(tile: str) -> int:
+    if tile == "Joker":
+        return 53
+    else:
+        colors = {"blue": 1, "red": 2, "yellow": 3, "black": 4}
+        color, number = tile.split("|")
+        return colors[color] * int(number)
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import re
+
+    with open(r"machine_learning\utils\sets.txt", "r") as f:
+        sets = [re.sub(r"\[|\s|\]\n", "", i).split(",") for i in f.readlines()]
+    sets_strings = [str(i) for i in sets if len(i) == 5 and i.count(str(53)) == 2]
+
+    print(len(np.unique(sets_strings)))
+    # blue, red, yellow, black, 53 for joker
+    print(np.floor_divide(15, 13))
+    print("Index:", tile_string2number("red|13"))
