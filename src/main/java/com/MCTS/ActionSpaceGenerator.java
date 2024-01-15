0581a452b336cca06b4ba0cf565628cbfc7300d6
Jakub_Suszwedyk <suszwedykkuba@gmail.com>
2024-01-15 12:15:32 +0100

    Created the method to sort the sets depending on the amount of tiles that are common between them and the board.

--- src/main/java/com/MCTS/ActionSpaceGenerator.java
+++ src/main/java/com/MCTS/ActionSpaceGenerator.java
@@ -1,9 +1,6 @@
 package com.MCTS;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 import javafx.css.SimpleStyleableDoubleProperty;
 
