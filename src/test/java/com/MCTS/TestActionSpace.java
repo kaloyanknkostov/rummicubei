fbf96eed7078425304c4165e57a32bcf9dee1682
Jakub_Suszwedyk <suszwedykkuba@gmail.com>
2024-01-16 11:37:44 +0100

    Changed the testing of the Sorting the sets

--- src/test/java/com/MCTS/TestActionSpace.java
+++ src/test/java/com/MCTS/TestActionSpace.java
@@ -18,6 +18,37 @@ public class TestActionSpace {
         board.add(boardSetTwo);
         ArrayList<Integer> boardAsList = new ArrayList<>(Arrays.asList(2, 3, 4, 6, 7, 8, 9));
         ActionSpaceGenerator generator = new ActionSpaceGenerator(board, hand);
-        System.out.println(generator.sortPossibleSets(generator.getPossibleSets(), boardAsList) );
+        ArrayList<ArrayList<Integer>> result = generator.sortPossibleSets(generator.getPossibleSets(), boardAsList);
+        ArrayList<ArrayList<Integer>> expected_result = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(6, 7, 8, 9, 10)),
+                new ArrayList<>(Arrays.asList(5, 6, 7, 8, 9)),
+                new ArrayList<>(Arrays.asList(4, 5, 6, 7, 8)),
+                new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7)),
+                new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6)),
+                new ArrayList<>(Arrays.asList(6, 7, 8, 9)),
+                new ArrayList<>(Arrays.asList(7, 8, 9, 10, 11)),
+                new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5)),
+                new ArrayList<>(Arrays.asList(7, 8, 9, 10)),
+                new ArrayList<>(Arrays.asList(5, 6, 7, 8)),
+                new ArrayList<>(Arrays.asList(4, 5, 6, 7)),
+                new ArrayList<>(Arrays.asList(3, 4, 5, 6)),
+                new ArrayList<>(Arrays.asList(2, 3, 4, 5)),
+                new ArrayList<>(Arrays.asList(1, 2, 3, 4)),
+                new ArrayList<>(Arrays.asList(7, 8, 9)),
+                new ArrayList<>(Arrays.asList(6, 7, 8)),
+                new ArrayList<>(Arrays.asList(2, 3, 4)),
+                new ArrayList<>(Arrays.asList(8, 9, 10, 11, 12)),
+                new ArrayList<>(Arrays.asList(8, 9, 10, 11)),
+                new ArrayList<>(Arrays.asList(8, 9, 10)),
+                new ArrayList<>(Arrays.asList(5, 6, 7)),
+                new ArrayList<>(Arrays.asList(4, 5, 6)),
+                new ArrayList<>(Arrays.asList(3, 4, 5)),
+                new ArrayList<>(Arrays.asList(1, 2, 3)),
+                new ArrayList<>(Arrays.asList(9, 10, 11, 12)),
+                new ArrayList<>(Arrays.asList(9, 10, 11)),
+                new ArrayList<>(Arrays.asList(10, 11, 12))
+        ));
+        //System.out.println(result);
+        assertEquals(result, expected_result);
     }
 }
