98781461bd49c8082b895e714c394f2c21c37b14
Frederik Grüneberg <agent21@alpenjodel.de>
2024-01-19 13:32:08 +0100

    Add working one hot encoding for all

--- data/create_training_data.py
+++ data/create_training_data.py
@@ -10,26 +10,30 @@ from rich import print
 
 def one_hot_encoding_rack(input):
     output = [0] * 53 # 53 tiles are in the board
-    print(input)
     for tile in input:
         output[tile] = 1
+    return output
+
+def one_hot_encoding_board(board):
+    output = [0] * 679 # number of sets
+    for set_in_board in board:
+        output[ALL_SETS.index(set_in_board)] = 1
     return output
 
 def encode_Tiles_2_number(input: str) -> str:
     if len(input) <= 0:
         return ""
-    colors = {"red": 1, "blue": 2, "yellow": 3, "black": 4}
+    colors = {"red": 0, "blue": 1, "yellow": 2, "black": 3}
     for color in colors:
-        print("Input:", input, len(input))
         input = re.sub(color, str(colors[color]), input)
     if re.search(";", input): # if its a board
         return [[
-                int(tile.split("|")[0]) * int(tile.split("|")[1]) - 1
+                int(tile.split("|")[0]) * 13 + int(tile.split("|")[1])
                 # to reach zero (index)
                 for tile in re.findall(r"\d\|\d+", board_set)]
                 for board_set in input.split(";")]
     input = [
-        int(tile.split("|")[0]) * int(tile.split("|")[1]) - 1  # to reach zero (index)
+        int(tile.split("|")[0]) * 13 + int(tile.split("|")[1])  # to reach zero (index)
         for tile in re.findall(r"\d\|\d+", input)
     ]
     return input
@@ -48,6 +52,9 @@ def create_training_data(src: str):
     print(df.columns)  # Be careful of blanks in the first line!
     df["Board"] = df["Board"].apply(lambda x: "" if x == "Empty Board" else x)
     df["Board"] = df["Board"].apply(encode_Tiles_2_number)
+    print(df)
+    df["Board"] = df["Board"].apply(one_hot_encoding_board)
+
     # df.insert(2, "Player1", value=None)
     # df.insert(3, "Player2", value=None)
     print(df)
@@ -56,7 +63,7 @@ def create_training_data(src: str):
     df["Player2"] = df["Player2"].apply(encode_Tiles_2_number)
 
     df = df.drop("PlayersHands", axis=1)
-
+    print(df)
     df["Player1"] = df["Player1"].apply(one_hot_encoding_rack)
     df["Player2"] = df["Player2"].apply(one_hot_encoding_rack)
 
@@ -68,10 +75,14 @@ def create_training_data(src: str):
         # get the first n lines for the first n boards (ordered by move number asc)
 
         pass
+    print(df["Board"].iloc[-1].index(1))
     return df
 
 
 if __name__ == "__main__":
+    with open(r"data\allSets.txt", "r") as f:
+        ALL_SETS = [[int(x) for x in re.sub(r"\[|\]|\n", "", i).split(", ")]
+                    for i in f.readlines()]
     src = r"data\raw_data\Game0.csv"
     dst = r"data\training_data\test.csv"
     print(encode_Tiles_2_number("black|13"))
