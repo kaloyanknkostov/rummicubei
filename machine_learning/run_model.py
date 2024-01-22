3115b076fbe902e8103bc0799786f903f56a9a8c
Frederik Grüneberg <agent21@alpenjodel.de>
2024-01-22 12:31:58 +0100

    Finish run model file

--- machine_learning/run_model.py
+++ machine_learning/run_model.py
@@ -1,12 +1,17 @@
 import argparse
 from ast import literal_eval
 import re
+import os
+
+# from rich import print
+import torch
+from model import TilePredNet
 
 
 def one_hot_encoding_rack(input) -> list[int]:
     output = [0] * 53  # 53 tiles are in the board
     for tile in input:
-        output[tile - 1] = 1  # -1 as the index starts at 0
+        output[int(tile) - 1] = 1  # -1 as the index starts at 0
     return output
 
 
@@ -18,6 +23,15 @@ def one_hot_encoding_board(board) -> list[int]:
 
 
 if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # print(f"Using {device} device")
+    MODEL_PATH = os.path.join(
+        r"machine_learning",
+        "model_files",
+        "20240122-09_05_14_model_epochs_39_batch_size1_l_0."
+        + "0005_loss_CrossEntropyLoss_opt_type.pt",
+    )
+
     with open(r"data\allSets.txt", "r") as f:
         ALL_SETS = [
             [int(x) for x in re.sub(r"\[|\]|\n", "", i).split(", ")]
@@ -26,21 +40,29 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Execute TilePred Model", description="", epilog=""
     )
-    parser.add_argument("-t", "--target", default=[])
-    parser.add_argument("-r", "--rack", default=[])
-    parser.add_argument("-b1", "--board1", default=[])  # Current board
-    parser.add_argument("-b2", "--board2", default=[])
-    parser.add_argument("-b3", "--board3", default=[])
-    parser.add_argument("-b4", "--board4", default=[])
-    parser.add_argument("-b5", "--board5", default=[])
-    parser.add_argument("-b6", "--board6", default=[])
-    parser.add_argument("-b7", "--board7", default=[])
-    parser.add_argument("-b8", "--board8", default=[])
+    parser.add_argument("-r", "--rack", default="[]")
+    parser.add_argument("-b1", "--board1", default="[]")  # Current board
+    parser.add_argument("-b2", "--board2", default="[]")
+    parser.add_argument("-b3", "--board3", default="[]")
+    parser.add_argument("-b4", "--board4", default="[]")
+    parser.add_argument("-b5", "--board5", default="[]")
+    parser.add_argument("-b6", "--board6", default="[]")
+    parser.add_argument("-b7", "--board7", default="[]")
+    parser.add_argument("-b8", "--board8", default="[]")
 
     args = parser.parse_args()
-    print(literal_eval(args.target)[0])
-    print(literal_eval(args.board1)[0])
-    print(vars(args))
-    for i in vars(args):
-        print(i)
-8
+    args_dict = vars(args)
+    args_dict["rack"] = one_hot_encoding_rack(literal_eval(args_dict["rack"]))
+    for board in args_dict:
+        if board != "rack":
+            args_dict[board] = one_hot_encoding_board(literal_eval(args_dict[board]))
+
+    X = torch.cat([torch.Tensor(args_dict[key]) for key in args_dict]).to(device)
+
+    model = TilePredNet(n_rounds=8, n_out=53, n_sets=679, dropout=0.2)
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device(device)))
+    model.to(device)
+
+    preds = model(X)
+    results = preds.round(decimals=3).tolist()
+    print(results)
